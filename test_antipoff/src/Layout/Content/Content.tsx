0f65fa6a238a15836886725851110c7d7656e6c2
andrey_ka <andrey.kashaev2018@yandex.ru>
2023-02-26 15:51:57 +0300

    add save likes

--- test_antipoff/src/Layout/Content/Content.tsx
+++ test_antipoff/src/Layout/Content/Content.tsx
@@ -2,27 +2,23 @@ import React, {useLayoutEffect, useState} from 'react';
 import {Header} from "./Header";
 import {PersonsList} from "./PersonsList";
 import {useDispatch, useSelector} from "react-redux";
-import {getUsersAction} from "../../store/usersReducer";
 import {Route, Routes} from "react-router";
 import {PersonPage} from "./PersonPage";
 
 export function Content() {
     const [target, setTarget] = useState(true)
     const [textCard, setTextCard] = useState('')
-    const dispatch: any = useDispatch()
+    // const dispatch: any = useDispatch()
     // @ts-ignore
     const cardData = useSelector(state => state.personCardReducer.card)
 
     useLayoutEffect(() => {
         if (target) {
-            dispatch(getUsersAction())
+            // dispatch(getUsersAction())
             setTarget(false)
         }
         if (cardData) {
             setTextCard(cardData.data.email)
-        }
-        if(localStorage.likes === undefined) {
-            localStorage.likes = JSON.stringify([])
         }
     }, [cardData])
     return (
--- test_antipoff/src/Layout/Content/PersonsList/CardItem/CardItem.tsx
+++ test_antipoff/src/Layout/Content/PersonsList/CardItem/CardItem.tsx
@@ -7,6 +7,7 @@ import {useDispatch} from "react-redux";
 import {getFullCard} from "../../../../store/personCardReducer";
 import {Button} from "../../../../UI/Button";
 import {likesArr} from "../../../../../types/likeTypes";
+import {useLikes} from "../../../../hooks/useLikes";
 
 interface ICard {
     card: userType
@@ -14,39 +15,14 @@ interface ICard {
 
 export function CardItem({card}: ICard) {
     const dispatch: any = useDispatch()
+    const [idState, setIdState] = useState(-1)
     const [isActive, setIsActive] = useState(false)
-    let arrLikes: likesArr = JSON.parse(localStorage.likes)
+    const arrStateButtons =  useLikes(isActive, idState)
+    const [stateBtn] = arrStateButtons.filter(value => value.id === card.id)
     const likeChange = () => {
-
+        setIdState(card.id)
         setIsActive(!isActive)
-        // if (arrLikes.filter(value => value.id === card.id).length === 0) {
-        //     console.log('nUl')
-        //     arrLikes.push({id: card.id, isLike: !isActive})
-        //     localStorage.likes = JSON.stringify([...JSON.parse(localStorage.likes), ...arrLikes])
-        // } else {
-        //     for (let i = 0; i < arrLikes.length; i++) {
-        //         if (arrLikes[i].id === card.id) {
-        //             arrLikes[i].isLike = !arrLikes[i].isLike
-        //         }
-        //     }
-        //     localStorage.likes = JSON.stringify([...arrLikes])
-        //     console.log('NOT Nul', arrLikes)
-        //     arrLikes = [...JSON.parse(localStorage.likes)]
-        // }
-
     }
-    // useEffect(() => {
-    //     if (localStorage.likes === undefined) {
-    //         localStorage.likes = JSON.stringify([])
-    //     } else {
-    //         if (JSON.parse(localStorage.likes).indexOf(card.id) === -1) {
-    //             setIsActive(false)
-    //         } else {
-    //             setIsActive(true)
-    //         }
-    //     }
-    // }, [])
-
     const onClickCard = () => {
         dispatch(getFullCard(card.id))
     }
@@ -58,7 +34,7 @@ export function CardItem({card}: ICard) {
             </Link>
             <div className={styles.positionBtn}>
                 <Button textBtn={''} style={{backgroundColor: 'var(--greyLightF8)', padding: '8px', borderRadius: ''}}
-                        icon={isActive ? 'IconLikeOn' : 'IconLikeOff'} click={likeChange}/>
+                        icon={stateBtn?.isLike ? 'IconLikeOn' : 'IconLikeOff'} click={likeChange}/>
             </div>
         </li>
     );
